dbd27cfcee0cd70bd64885e88562a3ec2512c151
Amanda <amanda.wikstrom@hotmail.com>
2022-02-23 14:06:27 +0100

    fix nav with cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiShoppingBag} from 'react-icons/fi';
 
 const Header = () => {
   return (
     <nav>
       <Link to='/'>Products</Link>
-      <Link to='/cart'>Cart</Link>
+      <Link to='/cart' className='iconLink'><FiShoppingBag className='cartIcon' />(0)</Link>
     </nav>
   )
 }
