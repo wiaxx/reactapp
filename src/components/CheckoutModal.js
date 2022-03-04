786b1d97357bd0da71091871f16b3b8592dc9c3c
unknown <tom.eriksson88@gmail.com>
2022-03-04 16:24:57 +0100

    fuck redirect, kör knapp istället.

--- src/components/CheckoutModal.js
+++ src/components/CheckoutModal.js
@@ -1,6 +1,38 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const CheckoutModal = ({ showModal, setShowModal }) => {
+	return (
+		<>
+			{showModal ? (
+				<Container>
+					container{" "}
+					<CheckoutModalContent>
+						<CheckoutModalHeader>
+							<CheckoutModalTitle>
+								<h4>Modal Title</h4>
+							</CheckoutModalTitle>
+						</CheckoutModalHeader>
+						<CheckoutModalBody>
+							<p>Thank you for your order</p>
+
+							<h3>Your ordernumber is:</h3>
+
+							<p>Closing this window will redirect you to the homepage</p>
+						</CheckoutModalBody>
+
+						<CheckoutModalFooter>
+							<Link to={`/`}>
+								<button> Close</button>
+							</Link>
+						</CheckoutModalFooter>
+					</CheckoutModalContent>
+				</Container>
+			) : null}
+		</>
+	);
+};
 
 const Container = styled.div`
 	position: fixed;
@@ -14,8 +46,27 @@ const Container = styled.div`
 	justify-content: center;
 `;
 
-const CheckoutModal = ({ showModal, setShowModal }) => {
-	return <>{showModal ? <Container>Modal</Container> : null}</>;
-};
+const CheckoutModalContent = styled.div`
+	width: 500px;
+	background-color: #fff;
+`;
+
+const CheckoutModalFooter = styled.div`
+	padding: 10px;
+`;
+
+const CheckoutModalHeader = styled.div`
+	padding: 10px;
+`;
+
+const CheckoutModalTitle = styled.div`
+	margin: 0;
+`;
+
+const CheckoutModalBody = styled.div`
+	padding: 10px;
+	border-top: 1px solid #eee;
+	border-bottom: 1px solid #eee;
+`;
 
 export default CheckoutModal;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -37,7 +37,7 @@ const Products = ({ addCartItem }) => {
         ))
        
       }
-    </MainContainer >
+      </MainContainer >
   )
 }
 
